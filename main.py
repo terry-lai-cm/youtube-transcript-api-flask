ae28076dbb0e8380305446d5b87885110dc3bd08
terry-lai-cm <terry_lai@cmoney.com.tw>
2025-05-18 15:00:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from app import app
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))  # Cloud Run 要求用 PORT 環境變數
+    app.run(host='0.0.0.0', port=port)
